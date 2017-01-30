0d6cb68322c789a66995c4f2c6ebac0ac8578625
John <jw4429@nyu.edu>
2017-01-30 10:57:28 -0500

    Added Sniper game to Repo
    Fuck yeah, I did it.

---
+++ Sniper Game/Assets/Scripts/Targeting.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Targeting : MonoBehaviour { 
+
+    public float MovementSpeed; //public number to allow for chnages during gameplay
+
+	void Start ()
+    {
+	
+	}
+	
+	
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y + MovementSpeed, 0); //Upward Movement
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y - MovementSpeed, 0); //Downward Movement
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Transform>().position = new Vector3(transform.position.x + MovementSpeed, transform.position.y, 0); //Rightward Movement
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Transform>().position = new Vector3(transform.position.x - MovementSpeed, transform.position.y, 0); //Leftward Movement
+        }
+
+    }
+}
