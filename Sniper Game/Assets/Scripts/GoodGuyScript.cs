f5dd8eabee9d39a963c379ab7666a7a9fc373d60
John <jw4429@nyu.edu>
2017-02-01 12:18:53 -0500

    Changed Everything
    organized, rewrote my movement script, and make a good guy script.

---
+++ Sniper Game/Assets/Scripts/GoodGuyScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GoodGuyScript : MonoBehaviour
+{
+    //Script for basic good guy 
+
+    Text StopShootingText;
+
+    public bool OverlappedGoodGuy = false; //boolean to check if is the reticle is on the good guy
+
+    void Start ()
+    {
+        StopShootingText = GetComponent<Text>();
+    }
+	
+	
+	void Update ()
+    {
+       if (OverlappedGoodGuy = true &&
+           Input.GetKeyDown(KeyCode.Space)) //Checks if it is on the target 
+       {
+            StopShootingText.text = "That's not the target!";
+       }
+
+    }
+
+       void OnTriggerEnter2D(Collider2D Object) //Checking it has collided with the reticle
+       {
+         OverlappedGoodGuy = true; //It has overlapped
+       }
+    
+
+}
+
--- Sniper Game/Assets/Scripts/Targeting.cs
+++ Sniper Game/Assets/Scripts/Targeting.cs
@@ -1,33 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Targeting : MonoBehaviour { 
+public class Targeting : MonoBehaviour // Targeting script. Sets the boundries and gives the player movement input.
+{ 
 
     public float MovementSpeed; //public number to allow for chnages during gameplay
+    public float Leftbound = -25; //Left boundry
+    public float Rightbound = 25; //Right boundry
+    public float Topbound = 10; //Top boundry
+    public float Bottombound = -10; //Bottom boundry
+    Rigidbody2D rb; //establishes the ridgidbody 
 
 	void Start ()
     {
-	
+	    rb = GetComponent<Rigidbody2D>(); //gets the ridgidbody 
 	}
 	
 	
 	void Update ()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W)) //Upward Movement
         {
-            GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y + MovementSpeed, 0); //Upward Movement
+           rb.MovePosition(new Vector3(transform.position.x, transform.position.y + MovementSpeed, 0)); 
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S)) //Downward Movement
         {
-            GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y - MovementSpeed, 0); //Downward Movement
+            rb.MovePosition(new Vector3(transform.position.x, transform.position.y - MovementSpeed, 0)); 
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D)) //Rightward Movement
         {
-            GetComponent<Transform>().position = new Vector3(transform.position.x + MovementSpeed, transform.position.y, 0); //Rightward Movement
+            rb.MovePosition(new Vector3(transform.position.x + MovementSpeed, transform.position.y, 0)); 
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A)) //Leftward Movement
         {
-            GetComponent<Transform>().position = new Vector3(transform.position.x - MovementSpeed, transform.position.y, 0); //Leftward Movement
+            rb.MovePosition(new Vector3(transform.position.x - MovementSpeed, transform.position.y, 0)); 
+        }
+        if (transform.position.x < Leftbound) //Left boundry 
+        {
+            rb.MovePosition(new Vector3(Leftbound, transform.position.y, 0));
+        }
+        if (transform.position.x > Rightbound) //Right boundry
+        {
+            rb.MovePosition(new Vector3(Rightbound, transform.position.y, 0));
+        }
+        if (transform.position.y < Bottombound) //Bottom boundry
+        {
+            rb.MovePosition(new Vector3(transform.position.x, Bottombound, 0));
+        }
+        if (transform.position.y > Topbound) //Top boundry
+        {
+            rb.MovePosition(new Vector3(transform.position.x, Topbound, 0));
         }
 
     }
